96ead34c7033b16e4ef9d3864f739ba18c22ad2d
Pawel Ryterski <Pawel Ryterski>
2016-10-08 22:47:56 +0200

    Initial commit

---
+++ Source/BuddyProject/BuddyProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuddyProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuddyProject, "BuddyProject" );
---
+++ Source/BuddyProject/BuddyProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuddyProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUDDYPROJECT_API ABuddyProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
