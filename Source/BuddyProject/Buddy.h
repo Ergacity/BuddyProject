c483c66f5f15d8e46ddef897898395f40f46b42e
Ryty <rytpaw@gmail.com>
2016-10-11 16:35:17 +0200

    NewBuddy-kod

---
+++ Source/BuddyProject/Buddy.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PlayerCharacter.h"
+#include "Buddy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUDDYPROJECT_API ABuddy : public APlayerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
